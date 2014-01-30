2705500b578677516ad33298b2155585c32f9d4d
alec resnick <aresnickety@gmail.com>
2014-01-30 02:24:15 -0500

    Added basic https://www.apple.com/30-years/ functionality via JS and pulled into separate file.

---
+++ timeline.js
@@ -0,0 +1,83 @@
+// Utility function to grab style properties when unset
+function getStyleProp(elem, prop){
+    if(window.getComputedStyle)
+        return window.getComputedStyle(elem, null).getPropertyValue(prop);
+    else if(elem.currentStyle) return elem.currentStyle[prop]; //IE
+}
+
+// 20ms event loop to update a global mouseX, mouseY position and handle scroll detection
+var mouseX = null;
+var mouseY = null;
+var scrollTimeline = null;
+window.onmousemove = function(event) {
+	mouseX = event.clientX;
+	mouseY = event.clientY;
+	if (!scrollTimeline) {
+		scrollTimeline = window.setInterval(scroll, 20);
+	}
+};
+
+// Global access to all the project li's
+var projects = document.querySelectorAll('#timeline li.project');
+projects = Array.prototype.slice.call(projects);
+for (i = 0; i < projects.length; i++) {
+	projects[i].onmouseover = slide; // Slide via highlight
+}
+
+// Global access to the timeline container
+var timeline = document.querySelector("#timeline");
+timeline.onmouseout = unslide; // Undo slides when out of timeline
+
+
+function scroll(event) {
+	var buffer = 200;
+
+	var distanceToCenter = Math.abs(window.innerWidth/2-mouseX);
+	var speed = distanceToCenter/(window.innerWidth/2);
+	if (mouseX < buffer) {
+		scrollLeft(speed);
+	}
+	else if ((window.innerWidth - mouseX) < buffer) {
+		scrollRight(speed);
+	}
+	else {
+		window.clearInterval(scrollTimeline);
+		scrollTimeline = null;
+	}
+}
+
+function scrollLeft(speed) {
+	scrollRight(speed*-1);
+}
+
+function scrollRight(speed) {
+	var leftPixels = parseInt(getStyleProp(timeline, 'left'), 10);
+	var toShift = Math.pow(speed,3)*10;
+	var newLeft = leftPixels - toShift;
+	if (newLeft >= -1400 && newLeft  <= 0) {
+		timeline.style.left = newLeft + 'px';
+	}
+}
+
+function slide(event)
+{
+	this.classList.add("hover");
+	var description = document.querySelector(".hover .description");
+	for (i = 0; i < projects.length; i++) {
+		if (i <= projects.indexOf(this)) {
+			projects[i].classList.add("slideLeft");
+			projects[i].classList.remove("slideRight");
+		}
+		else {
+			projects[i].classList.add("slideRight");
+			projects[i].classList.remove("slideLeft");
+		}
+	}
+}
+
+function unslide(event) {
+	for (i = 0; i < projects.length; i++) {
+		projects[i].classList.remove("slideRight");
+		projects[i].classList.remove("slideLeft");
+	}
+}
